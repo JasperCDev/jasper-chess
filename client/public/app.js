07fabc52b61f1f0fa244911b1815e174c4740487
JasperCDev <jasperischauvin@gmail.com>
2021-06-21 20:59:51 -0500

    added pieces to board

--- client/public/app.js
+++ client/public/app.js
@@ -1,2 +1,38 @@
 "use strict";
-console.log("hello");
+var chessSquares = document.querySelectorAll(".ChessSquare");
+console.log(chessSquares);
+var ChessBoard = /** @class */ (function () {
+    function ChessBoard() {
+        this.board = this.setUpBoard();
+        this.renderBoard();
+    }
+    ChessBoard.prototype.setUpBoard = function () {
+        return [
+            ["r-black", "n-black", "b-black", "q-black", "k-black", "b-black", "n-black", "r-black"],
+            ["p-black", "p-black", "p-black", "p-black", "p-black", "p-black", "p-black", "p-black"],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            [null, null, null, null, null, null, null, null],
+            ["p-white", "p-white", "p-white", "p-white", "p-white", "p-white", "p-white", "p-white"],
+            ["r-white", "n-white", "b-white", "q-white", "k-white", "b-white", "n-white", "r-white"]
+        ];
+    };
+    ChessBoard.prototype.renderBoard = function () {
+        var _this = this;
+        Array.from(chessSquares).forEach(function (square, index) {
+            var rowIndex = Math.floor(index / 8);
+            var row = _this.board[rowIndex];
+            var indx = index % 8;
+            if (row[indx] === null) {
+                return;
+            }
+            var img = document.createElement("img");
+            img.src = "chessPieces/" + row[indx] + ".png";
+            square.appendChild(img);
+            console.log("here");
+        });
+    };
+    return ChessBoard;
+}());
+var chessBoard = new ChessBoard();
--- client/src/app.ts
+++ client/src/app.ts
@@ -1,1 +1,42 @@
-console.log("hello");
+const chessSquares = document.querySelectorAll(".ChessSquare");
+console.log(chessSquares);
+
+class ChessBoard {
+  board: Array<Array<string | null>>;
+
+  constructor() {
+    this.board = this.setUpBoard();
+    this.renderBoard();
+  }
+
+  setUpBoard(): Array<Array<string | null>> {
+    return [
+      ["r-black", "n-black", "b-black", "q-black", "k-black", "b-black", "n-black", "r-black"],
+      ["p-black", "p-black", "p-black", "p-black", "p-black", "p-black", "p-black", "p-black"],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      [null, null, null, null, null, null, null, null],
+      ["p-white", "p-white", "p-white", "p-white", "p-white", "p-white", "p-white", "p-white"],
+      ["r-white", "n-white", "b-white", "q-white", "k-white", "b-white", "n-white", "r-white"]
+    ];
+  }
+
+  renderBoard(): void {
+    Array.from(chessSquares).forEach((square, index) => {
+      let rowIndex = Math.floor(index / 8);
+      const row = this.board[rowIndex];
+      const indx = index % 8;
+        if (row[indx] === null) {
+          return;
+        }
+        const img = document.createElement("img");
+        img.src = `chessPieces/${row[indx]}.png`;
+        square.appendChild(img);
+        console.log("here");
+    });
+  }
+
+}
+
+const chessBoard = new ChessBoard();
